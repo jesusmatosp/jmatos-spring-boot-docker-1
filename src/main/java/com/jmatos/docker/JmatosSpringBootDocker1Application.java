a0baebec202073033cf9c7e9a9e369f3c6f9f9d4
Jesus Matos <jesus.matosp@gmail.com>
2020-07-09 23:52:58 -0500

    first commit

---
+++ src/main/java/com/jmatos/docker/JmatosSpringBootDocker1Application.java
@@ -0,0 +1,21 @@
+package com.jmatos.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JmatosSpringBootDocker1Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Jesus ! Welcome Docker World!!!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JmatosSpringBootDocker1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmatos/docker/JmatosSpringBootDocker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmatos.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmatosSpringBootDocker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
